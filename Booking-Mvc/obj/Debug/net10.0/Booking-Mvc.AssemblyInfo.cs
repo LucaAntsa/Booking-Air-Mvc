8417e38941a92644ef5b7612938591538b60494e
nitroo4 <mickarzk@gmail.com>
2026-03-17 13:04:19 +0100

    add: footer

--- Booking-Mvc/obj/Debug/net10.0/Booking-Mvc.AssemblyInfo.cs
+++ Booking-Mvc/obj/Debug/net10.0/Booking-Mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Booking-Mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+079b5331806653a12a0523d6868465af207ca79d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87ff2d090587cf0ca0703b2d96acfe9a79d94255")]
 [assembly: System.Reflection.AssemblyProductAttribute("Booking-Mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Booking-Mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
