e29090ca51a40738108da56cce9b2dc5514b8189
LucaAntsa <antsalucarabison@gmail.com>
2026-03-18 17:01:53 +0300

    fix: fix

--- Booking-Mvc/obj/Debug/net10.0/Booking-Mvc.AssemblyInfo.cs
+++ Booking-Mvc/obj/Debug/net10.0/Booking-Mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Booking-Mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ac2854f82376db8a03cfb351963b1eab16828ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4759b2392bf9802ff38d562bef6e40ebbd38bd00")]
 [assembly: System.Reflection.AssemblyProductAttribute("Booking-Mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Booking-Mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
