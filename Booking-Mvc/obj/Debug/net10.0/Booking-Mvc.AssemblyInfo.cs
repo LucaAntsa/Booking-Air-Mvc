51926bb14c43dab69efc72218db4dd6fd9f77f6c
nitroo4 <mickarzk@gmail.com>
2026-03-19 14:36:40 +0100

    add: profile

--- Booking-Mvc/obj/Debug/net10.0/Booking-Mvc.AssemblyInfo.cs
+++ Booking-Mvc/obj/Debug/net10.0/Booking-Mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Booking-Mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e12a1d6dcee1379816732d0af859d685c4f30cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c71616075221a1aff5eb05edc96b8807cbceada")]
 [assembly: System.Reflection.AssemblyProductAttribute("Booking-Mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Booking-Mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
