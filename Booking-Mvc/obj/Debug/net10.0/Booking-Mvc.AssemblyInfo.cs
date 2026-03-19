59474f2c991e3ae15fc60db11825263d27aacd3d
LucaAntsa <antsalucarabison@gmail.com>
2026-03-19 11:54:00 +0300

    fix: fix the login and register

--- Booking-Mvc/obj/Debug/net10.0/Booking-Mvc.AssemblyInfo.cs
+++ Booking-Mvc/obj/Debug/net10.0/Booking-Mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Booking-Mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+213edfeb30dcea271ce118a09d9f74aeee4cdc03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc561b86ed4ea8d2c3120a70d3687422b460a44b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Booking-Mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Booking-Mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
